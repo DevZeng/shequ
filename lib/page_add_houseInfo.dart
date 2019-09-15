736c4a3661052557852cbc2deddb84cc7a33e8d9
Zeng <zeng@Zengs-Mac-mini.local>
2019-09-15 22:19:36 +0800

    fiux

--- lib/page_add_houseInfo.dart
+++ lib/page_add_houseInfo.dart
@@ -49,6 +49,7 @@ class Page extends State<addHouseInfo> {
                   alignment: Alignment.center,
                   child: GestureDetector(
                     child: Container(
+                      child: houseInfo.imageAddress.length!=0?Container():Icon(Icons.add,size: 50,color: Colors.grey,),
                       width: 120,
                       height: 120,
                       decoration: BoxDecoration(
